c627cdbeb0e061c4908cca4b996d24b89b0457b9
Artem Golubev <lesovba@gmail.com>
2023-04-21 17:41:59 +0500

    Completed Task3

---
+++ Task3/Task3.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()); 
+
+if (number%2 == 0)
+{
+    Console.WriteLine($"Число {number} - чётное");
+}
+
+else 
+
+{ 
+    Console.WriteLine($"Число {number} - нечётное");
+}
