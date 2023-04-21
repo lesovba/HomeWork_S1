ee829e5f9d9266ab68ed48bf8f37248ae93d2ad5
Artem Golubev <lesovba@gmail.com>
2023-04-21 17:21:37 +0500

    Renamed Program.cs into Task2.cs

---
+++ Task2/Task2.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+if (number1>number2 && number1>number3)
+{
+    Console.WriteLine("Максимальное число равно " + number1);
+}
+
+if (number2>number1 && number2>number3)
+
+{ 
+    Console.WriteLine("Максимальное число равно " + number2);
+}
+
+if (number3>number1 && number3>number2) 
+{ 
+    Console.WriteLine("Максимальное число равно " + number3);
+}
